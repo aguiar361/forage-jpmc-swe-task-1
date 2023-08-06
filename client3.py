17b571aa63ff37315baa6b255173f0ff8a6f8485
aguiar361 <brandonjaguiar@gmail.com>
2023-08-06 16:33:25 -0400

    completed task 1

--- client3.py
+++ client3.py
@@ -31,18 +31,20 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    'error handler 0 division'
+    if(price_b == 0):
+        return
+    'else of error handler'
+    return price_a/price_b
 
 
 # Main
@@ -51,9 +53,11 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        'New structure to store price data'
+        price_dict={}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            price_dict[stock] = price;
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(price_dict["ABC"], price_dict["DEF"]))
